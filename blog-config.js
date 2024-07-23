80ccb9c0c8f9919c333751f35ad923f153bb4b6e
JJONSOO <pjs970616@naver.com>
2024-07-23 20:38:53 +0900

    chore : remove unused things

--- blog-config.js
+++ blog-config.js
@@ -1,16 +1,11 @@
 module.exports = {
-  title: "june",
-  description: "Hello :) I'm June who developed gatsby-starter-hoodie theme.",
-  author: "June",
+  title: "Joonsoo",
+  description: "Hello :) I'm Joonsoo Park",
+  author: "Joonsoo",
   siteUrl: "https://june.blog",
   links: {
-    github: "https://github.com/JJONSOO",
-    linkedIn: "https://linkedin.com",
-    email: "mailto:devhudi@gmail.com",
-    resume: "https://hudi.blog",
+    github: "https://github.com/JJONSOO"
   },
   utterances: {
-    repo: "JJONSOO/june.blog",
-    type: "pathname",
   },
 }
