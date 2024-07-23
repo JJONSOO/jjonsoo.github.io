f24276b74dbba1c6e6498b21169f36dea84f76b2
JJONSOO <pjs970616@naver.com>
2024-07-23 20:47:56 +0900

    chore : remove unused things

--- blog-config.js
+++ blog-config.js
@@ -2,10 +2,12 @@ module.exports = {
   title: "Joonsoo",
   description: "Hello :) I'm Joonsoo Park",
   author: "Joonsoo",
-  siteUrl: "https://june.blog",
+  siteUrl: "https://jjonsoo.github.io",
   links: {
     github: "https://github.com/JJONSOO"
   },
   utterances: {
+    repo: "JJONSOO/jjonsoo.github.io",
+    type: "pathname",
   },
 }
